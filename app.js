d773a96340261e3752dfe17e92435a87e7b58be0
jhishan <jhishan@gmail.com>
2016-11-14 20:04:01 -0500

    set up basic template, add views

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var passport = require('passport');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(3000, function () {
