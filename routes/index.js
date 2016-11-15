8626fe33be01f10abb588cd58628ffba55100f55
Avika Narula <an1520@nyu.edu>
2016-11-14 20:59:42 -0500

    register form

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,27 @@ var express = require('express'),
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	  console.log(req); 
+	  console.log(res);
       res.render('index');
+});
+
+router.get('/register', function(req, res, next){
+	res.render('register');
+});
+
+router.post('/register', function(req, res, next){
+	var firstName = req.body.firstName;
+	var lastName = req.body.lastName;
+	var email = req.body.email;
+	var password = req.body.password;
+	console.log(firstName);
+	console.log(lastName);
+	console.log(email);
+	console.log(password);
+
+	res.send("your information was received");
+
 });
 
 module.exports = router;
