3596bfe3918f889cc3806895d40465b031686836
Avika Narula <an1520@nyu.edu>
2016-11-14 20:08:44 -0500

    set up app.js, router, a bunch of stuff

--- app.js
+++ app.js
@@ -6,10 +6,65 @@ var passport = require('passport');
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'hbs');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+//set up session
+var session = require('express-session');
+var sessionOptions = {
+  secret: 'secret cookie thang (store this elsewhwere!)', 
+  resave: true, 
+  saveUninitialized: true
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sessionOptions));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//make req.user avaliable in every context
+app.use(function(req, res, next){
+  res.locals.user = req.user; 
+  next();
+});
+
+var routes = require('./routes/index');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
+
+
+
