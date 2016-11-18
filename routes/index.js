4860fbb05dffe181c9ff11f08fff40bfafa32be9
Avika Narula <an1520@nyu.edu>
2016-11-18 15:44:56 -0500

    ahhh

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,6 @@ var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	  console.log(req);
-	  console.log(res);
       res.render('index');
 });
 
@@ -18,28 +16,17 @@ router.get('/register', function(req, res, next){
 });
 
 router.post('/register', function(req, res, next){
-	var firstName = req.body.firstName;
-	var lastName = req.body.lastName;
-	var email = req.body.email;
-	var password = req.body.password;
-    console.log("1");
 	User.register(new User({
-                username: email,
-                email: email,
-                firstName: firstName,
-                lastName: lastName,
+                username: req.body.email,
+                email: req.body.email,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName
             }), req.body.password, function(err, user) {
                 if (err) {
-                    console.log(err);
-                    console.log("2");
                     res.send("an error occured");
                 } else {
-					// TODO send email to newly registered user
-                    console.log("3");
-                    //req.session.user = user;
                     passport.authenticate('local')(req, res, function() {
                         res.send('new user was created, check mongo console to see if the new user was created');
-                    	// TODO redirect to next page in onboarding after new doctor signs up
 					});
                 }
 			});
